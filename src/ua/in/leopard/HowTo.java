82a16b1f9b23b90b8fc774caa873bdd2f54b5c3a
Alexey <leopard.not.a@gmail.com>
2011-03-06 20:17:16 +0200

    Fix

---
+++ src/ua/in/leopard/HowTo.java
@@ -0,0 +1,12 @@
+package ua.in.leopard;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HowTo extends Activity {
+	   @Override
+	   protected void onCreate(Bundle savedInstanceState) {
+	      super.onCreate(savedInstanceState);
+	      setContentView(R.layout.howto);
+	   }
+}
