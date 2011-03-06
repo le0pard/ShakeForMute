581f4309b6892056465314f7bf9c4609c5c3e033
Alexey <leopard.not.a@gmail.com>
2011-03-06 19:38:33 +0200

    Fix

--- src/ua/in/leopard/ShakeForMute.java
+++ src/ua/in/leopard/ShakeForMute.java
@@ -2,14 +2,15 @@ package ua.in.leopard;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.media.AudioManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageButton;
 
@@ -120,6 +121,35 @@ public class ShakeForMute extends Activity implements OnClickListener {
 	}
 	
 	private void updateView(){
+		if (mySettings.getOnOffStatus()){
+			onOffButton.setImageResource(R.drawable.dashboard_button_on_off);
+		} else {
+			onOffButton.setImageResource(R.drawable.dashboard_button_off_on);
+		}
+		
 		vibrateCheckbox.setChecked(mySettings.getVirbation());
 	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+      super.onCreateOptionsMenu(menu);
+      MenuInflater inflater = getMenuInflater();
+      inflater.inflate(R.menu.menu, menu);
+      return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+      switch (item.getItemId()) {
+      	case R.id.menu_howto_button:
+         //startActivity(new Intent(this, EditPreferences.class));
+         return true;
+      	case R.id.menu_about_button:
+      	 //startActivity(new Intent(this, About.class));
+      	 return true;
+      	default:
+	     return super.onOptionsItemSelected(item);
+      }
+	}
+	
 }
